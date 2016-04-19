dd2821d8eb7999988e1891617312288d7e20ad3c
Adam Vigneaux <adamvigneaux@gmail.com>
2016-04-19 13:45:16 -0400

    Fix indentation and variable declarations

--- project5.cc
+++ project5.cc
@@ -26,50 +26,49 @@ bool eof () {
 	}
 }
 
-int main(int argc, char * argv[])
+int main(int argc, char *argv[])
 {
-	std::vector <Province> provinces;
-	int numProvinces;
-
+    std::vector<Province*> provinces;
+    int numProvinces;
 
     // Repeatedly read input from standard input
     while (!eof()) {
-		int numTowns, numRoads;
-        std::cin townName, originTown, destinationTown;
-		char type;
-		double distance;
+	int numTowns, numRoads;
+        std::string townName, originTown, destinationTown;
+        char type;
+        double distance;
 
         // first two std::cin calls get the number of towns and the number of roads
         std::cin >> numTowns;
         std::cin >> numRoads;
-        Province theProvince = new Province(numTowns, numRoads);
+        Province *theProvince = new Province(numTowns, numRoads);
 
-		// add the province to the vector of provinces
-		provinces.push_back(theProvince);
+        // add the province to the vector of provinces
+        provinces.push_back(theProvince);
+        
+        // only the first town is the capital
+        bool isCapital = true;
 
-		// only the first town is the capital
-		bool isCapital = true;
-
-		// add the towns to the province
+        // add the towns to the province
         for (int i = 0; i < numTowns; i++) {
             std::cin >> townName;
-            theProvince.addTown(townName, isCapital);
-			isCapital = false;
+            theProvince->addTown(townName, isCapital);
+            isCapital = false;
         }
 
-		// add the roads to the province
+        // add the roads to the province
         for (int i = 0; i < numRoads; i++) {
-			std::cin >> originTown;
-			std::cin >> destinationTown;
-			std::cin >> type;
-			std::cin >> distance;
+            std::cin >> originTown;
+            std::cin >> destinationTown;
+            std::cin >> type;
+            std::cin >> distance;
             theProvince.addRoad(originTown, destinationTown, type, distance);
         }
-		numProvinces++;
-	}
+        numProvinces++;
+    }
 
-	// for each province, print out information about cities and roads
-	for (int i = 0; i < numProvinces; i++) {
-		provinces[i].printAll();
-	}
+    // for each province, print out information about cities and roads
+    for (int i = 0; i < numProvinces; i++) {
+        provinces[i].printAll();
+    }
 }
